9f2d4ee94434d5fdefd1c71be7c1b02f46baf394
KratikSoni1104 <118619980+KratikSoni1104@users.noreply.github.com>
2023-07-14 20:35:43 +0530

    created homeContainer

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,12 +2,26 @@ import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import "./home.css";
 import Header from '../../components/header/Header';
+import Feature from '../../components/feature/Feature';
+import PropertyList from '../../components/propertyList/PropertyList';
+import FeaturedProperty from '../../components/featuredProperty/FeaturedProperty';
+import MailList from '../../components/mailList/MailList';
+import Footer from '../../components/footer/Footer';
 
 function Home() {
   return (
     <div>
         <Navbar />
         <Header />
+        <div className='homeContainer'>
+          <Feature />
+          <h1 className='homeTitle'>Browse By property type</h1>
+          <PropertyList />
+          <h1 className='homeTitle'>Homes Guests Love</h1>
+          <FeaturedProperty />
+          <MailList />
+          <Footer />
+        </div>
     </div>
   )
 }
